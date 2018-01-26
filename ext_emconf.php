84e0633fc109dfe43e003c08d8e2edcce561aa9b
Sven Külpmann <kuelpmann@team23.de>
2018-01-26 14:37:01 +0100

    prepared ext_emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,22 +11,22 @@
  ***************************************************************/
 
 $EM_CONF[$_EXTKEY] = array (
-  'title' => 'spamshield',
+  'title' => 'spamshield // patched',
   'description' => 'Universal invisible Spamshield for TYPO3',
   'category' => 'fe',
-  'version' => '7.0.0',
+  'version' => '8.0.0',
   'state' => 'stable',
   'uploadfolder' => false,
   'createDirs' => '',
   'clearcacheonload' => false,
-  'author' => 'Ronald P. Steiner, Hauke Hain, Christian Seifert',
-  'author_email' => 'ronald.steiner@googlemail.com, fearistic@gmail.com, christian-f-seifert@gmx.de',
+  'author' => 'Ronald P. Steiner, Hauke Hain, Christian Seifert, Sven Külpmann',
+  'author_email' => 'ronald.steiner@googlemail.com, fearistic@gmail.com, christian-f-seifert@gmx.de, kuelpmann@team23.de',
   'author_company' => '',
   'constraints' => 
   array (
     'depends' => 
     array (
-      'typo3' => '7.6.0-7.6.99',
+      'typo3' => '7.6.0-8.7.99',
     ),
     'conflicts' => '',
     'suggests' => 
